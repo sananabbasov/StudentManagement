09674988b7b94abd78cfac71899268d0ba9a7d34
sananabbasov <sanan_abbasov@list.ru>
2022-08-15 10:55:44 +0400

    Add project files.

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public bool ConfirmEmail { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public int LoginAttempt { get; set; }
+        public bool UserActive { get; set; }
+        public bool Gender { get; set; }
+    }
+}
---
+++ Entities/Concrete/Question.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Abstract;
+using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Question: IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Hit { get; set; }
+        public int Likes { get; set; }
+        public string PhotoUrl { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
